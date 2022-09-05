66336ea44c2aa5168ecbc17ef8f5a85d9bbe1adb
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-09-05 19:02:53 +0500

    task 64 is done

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -4,3 +4,26 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
+Console.Write("Enter the number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void NaturanNumsToN(int num)
+{
+    if (num == 1)
+    {
+        Console.Write($"{num} ");
+        return;
+    }
+    if (n > 1)
+    {
+        Console.Write($"{num}, ");
+        NaturanNumsToN(num - 1);
+    }
+    else
+    {
+        Console.Write($"{num}, ");
+        NaturanNumsToN(num + 1);
+    }
+}
+
+NaturanNumsToN(n);
